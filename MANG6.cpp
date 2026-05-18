de75d901f3d9c66c91b148c655310dcb028de923
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-18 22:07:11 +0700

    MANG6

---
+++ MANG6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+bool So9P(int n){
+	int can = sqrt(n);
+	return can * can == n ; 
+}
+bool SNT(int n){
+	for (int i = 2 ; i < n ; i++){
+		if (n % i == 0) return false ;
+	}
+	return n > 1 ; 
+}
+int main(){
+	int n ; 
+	cin >> n ; 
+	int a[n] ; 
+	
+	for (int i = 0 ;  i < n ; i++){
+		cin >> a[i] ;
+	}
+	for(int i=0;i<=0;i++){
+		if(So9P(a[i])) cout << a[i] << " " ; 
+	}
+	cout << endl ; 
+	for(int i=0;i<=0;i++){
+		if(SNT(a[i])) cout << a[i] << " " ; 
+	}
+	
+	
+}
