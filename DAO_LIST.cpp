375e2499655b8b5f58a27b091807db6516b247d9
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-19 12:57:39 +0700

    DAO_LIST list

--- DAO_LIST.cpp
+++ DAO_LIST.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
-#include <stack> 
+#include <bits/stdc++.h>
 using namespace std ; 
 int main(){
 	int n ; cin >> n ; 
 	int x ; 
-	stack<int> st ; 
-	while(n--){
-		cin >> x ;
-		st.push(x);
+	list<int> l ; 
+	for (int i = 0 ; i < n ; i++){
+		cin >> x ; 
+		l.push_back(x);
 	}
-	while(!st.empty()){
-		cout << st.top() <<" " ; 
-		st.pop();
+	l.reverse();
+	for (int x : l){
+		cout << x << " " ; 
 	}
 }
