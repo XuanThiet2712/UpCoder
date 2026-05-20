00768c95731fb714890b308d5177bb438bed8e8e
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-20 10:34:28 +0700

    MINIGAME20.1:SIGUP v2

--- MINIGAME20.1.cpp
+++ MINIGAME20.1.cpp
@@ -6,24 +6,9 @@ int main(){
 	string s ; 
 	for (int i = 0 ; i < n ; i++){
 		cin >> s ; 
-		auto it =  find(v.begin() , v.end() , s ) ; 
-		if (it == v.end()) {
-			cout << "OK\n" ; 
-			v.push_back(s);
-		}
-		else {
-			int count = 1 ; 
-			string str ; 
-			while(true){
-				str = s + to_string(count);
-				if(find(v.begin(), v.end() , str ) == v.end()){
-					cout <<str<<endl;
-					v.push_back(str);
-					break;
-				}
-				count++;
-			}
-		}
+		if(find(v.begin() , v.end() , s) == v.end()) cout << "OK\n" ;
+		else cout << s << count(v.begin(),v.end() , s) << endl ; 
+		v.push_back(s);
 	}
 	
 }
