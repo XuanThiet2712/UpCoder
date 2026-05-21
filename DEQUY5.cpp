e26625ac0d35702406d1f35d2a696e4c7d8fa4be
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-21 22:46:20 +0700

    DEQUY5

---
+++ DEQUY5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std ; 
+
+double gt(int n ){
+	if(n == 1 || n == 0 ) return 1 ; 
+	return n * gt(n - 1) ; 
+}
+
+double S(int n , int x){
+	if(n < 0 ) return 1 ;	
+	return pow(x , 2*n + 1 )/gt(2*n + 1) + S(n - 1 , x ) ; 
+}
+
+int main(){
+	int n , x ; cin >> n >> x ; 
+	cout <<roundf(S(n , x) * 1000) / 1000 ; 
+}
