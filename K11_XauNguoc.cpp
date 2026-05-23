1f5c96df973994bda536eb235e51ce342b58741d
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-23 23:01:21 +0700

    K11_XauNguoc v2 reverse

--- K11_XauNguoc.cpp
+++ K11_XauNguoc.cpp
@@ -3,7 +3,6 @@ using namespace std ;
 int main(){
 	string s ; 
 	getline(cin , s); 
-	for(int i = s.size() - 1 ; i >= 0 ; i--){
-		cout << s[i] ;
-	}
+	reverse(s.begin() , s.end());
+	cout << s ; 
 }
