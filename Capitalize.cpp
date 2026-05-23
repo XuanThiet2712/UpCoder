8673fee050ad94806a68da7488f44b01068f375c
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-23 23:11:48 +0700

    Capitalize

---
+++ Capitalize.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+int main(){
+	string s ; 
+	while(cin >> s ){
+		for(int i = 0 ; i < s.size() ; i++){
+			if(i == 0 ) s[0] = toupper(s[0]);
+			else s[i] = tolower(s[i]);
+		}
+		cout << s << " " ; 
+	}
+
+
+}
