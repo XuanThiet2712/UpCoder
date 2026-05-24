bb83ff23b43eb2b013db07c3364bcc1ed121b3b8
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-24 10:21:48 +0700

    DEMCHUOI2 err 5/6 fixed

--- DEMCHUOI2.cpp
+++ DEMCHUOI2.cpp
@@ -4,6 +4,8 @@ int main(){
 	string s1 , s2 ; 
 	getline(cin , s1) ; 
 	getline(cin , s2) ; 
+	transform(s1.begin() , s1.end() ,s1.begin() ,  ::tolower);
+	transform(s2.begin() , s2.end() ,s2.begin() ,  ::tolower);
 	int count = 0 ; 
 	size_t pos = s2.find(s1 , 0) ; 
 	while(pos != string::npos){
