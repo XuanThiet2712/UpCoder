3b6ead397a2334695174ae0f7c2220f7c886b903
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-24 11:22:09 +0700

    [LTNC]Final04

--- [LTNC]Final04.cpp
+++ [LTNC]Final04.cpp
@@ -2,13 +2,18 @@
 using namespace std ; 
 int main(){
 	string mTP[6] = {"04" , "08" , "059" , "061" , "0511" , "0710"};
-	string tenTP[6] = {"Ha Noi" , "TPHCM" , "Gia Lai" , "Da Nang" , "Can Tho"};
+	string tenTP[6] = {"Ha Noi" , "TPHCM" , "Gia Lai" ,"Dong Nai" , "Da Nang" , "Can Tho"};
 	string sdt ; cin >> sdt ; 
-	string tmp = sdt.substr(0,3);
+	bool isAlpha = any_of(sdt.begin(), sdt.end(), ::isalpha);
+	if(isAlpha || sdt.size() != 10){
+		cout << "Khong hop le" ;
+		return 0 ; 
+	}
+	string tmp = sdt.substr(0,4);
 	bool valid = false ; 
 	for(int i=0;i<6;i++){
-		if(tmp.size() != mTP[i].size()) continue ; 
-		if(tmp == mTP[i]){
+		size_t pos = tmp.find(mTP[i]) ;
+		if(pos != string::npos && pos == 0 ){
 			cout << tenTP[i] << " - " << sdt.substr(mTP[i].size());
 			valid = true ; 
 			return 0 ; 
