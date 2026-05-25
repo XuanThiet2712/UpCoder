b433ea85612c82c45e6ffeb082e3dc425db6431f
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-25 23:28:57 +0700

    BIT03 err 4/10

---
+++ BIT03.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std ; 
+int main(){
+	int k ,mask , q ; 
+	cin >> k >> mask >> q; 
+	int a[k] ;
+	for (int i = k-1 ; i >= 0 ; i--){
+		a[i] = mask % 2 ; 
+		mask /= 2 ; 
+	}
+
+	while(q--){
+		bool check = 1 ; 
+		for(int i=0;i<k;i++){
+			if(a[i] != 0 ){
+				a[i] = 0 ;
+				cout << i << " " ;
+				check = 0 ; 
+				break ; 
+			}
+		}
+		if(check) cout << -1 << " " ; 
+	}
+
+}
