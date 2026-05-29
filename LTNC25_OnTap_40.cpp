d72475f8a69cda68bbd360c385d15bef4d4d2a30
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-29 23:42:51 +0700

    LTNC25_OnTap_40 err fixed

--- LTNC25_OnTap_40.cpp
+++ LTNC25_OnTap_40.cpp
@@ -1,13 +1,24 @@
 #include <bits/stdc++.h>
-using namespace std ;
-int main(){
-	string s , tmp ; 
-	getline(cin , s) ;
-	stringstream ss(s) ; 
-	int count = 0 ; 
-	while(ss >> tmp ){
-		count++;
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin, s);
+	int count = 0;
+	bool inWord = false;	
+	for (char c : s) {		
+		if (isalpha(c)) {
+			if (!inWord) {
+				count++;
+				inWord = true;
+			}
+		}
+		else {
+			inWord = false;
+		}
 	}
-	cout << count ; 
 	
+	cout << count;
+	
+	return 0;
 }
