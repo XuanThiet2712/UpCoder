081147385e1708d32d0ae75d03c46319dc1d0b21
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-29 18:57:28 +0700

    Demchun

---
+++ Demchun.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+int main(){
+	string s ;
+	getline(cin , s ) ;
+	int ct = 0, ch  = 0; 
+	for(char c : s){
+		if(c >= 'a' && c <= 'z') ct++;
+		else ch++;
+	}
+	cout << "Chữ hoa: "<<ch << endl ;
+	cout << "Chữ thường: "<<ct ;
+}
