eb6ad1a77cb4393afbaaf1f7d9444d87cd8f801c
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-05-30 22:56:25 +0700

    TEST_LTCB_CK_2 v2

--- TEST_LTCB_CK_2.cpp
+++ TEST_LTCB_CK_2.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-using namespace std ; 
-int main(){
+using namespace std;
+
+int main() {
 	int a[300];
-	int n = 0 ; 
-	int tmp ; 
-	while(cin >> tmp){
-		int check = 0 ; 
-		for(int i = 0 ; i < n ; i++){
-			if(tmp == a[i]) check = 1 ;  
-		}
-		if(check == 0 ){
-			a[n] = tmp ; 
-			n++;
-		}
-		
+	int n = 0;
+	int tmp;	
+	while (cin >> tmp) {		
+		for (int i = 0; i < n; i++) {
+			if (tmp == a[i])
+				goto duplicate; 
+		}		
+		a[n++] = tmp; 		
+		duplicate:;
 	}
-	for (int i = 0 ; i < n ; i++ ) 
-		cout << a[i] << "  " ;
+	
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
 }
