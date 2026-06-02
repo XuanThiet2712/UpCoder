610c7fc13dfaa366436ec0d7ae9e596c7f6ffcfa
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-06-02 23:11:27 +0700

    LTNC25_OnTap_20 err 5/10

---
+++ LTNC25_OnTap_20.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+using namespace std ; 
+int main(){
+	string s ; cin >> s ; 
+	stack<char> st ; 
+	for(char c : s){
+		if(c == '(')
+			st.push(c);
+		else if(c == ')'){
+			if(st.top() == '(')
+				st.pop();
+			else {
+				cout << "NO" ;
+				return 0; 
+			}
+		}
+	}
+	cout << (st.empty() ? "YES" : "NO");
+	
+}
