2d2067695a502d1a8e731596f877fdf5912d78f4
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-06-03 18:30:38 +0700

    Hexa

---
+++ Hexa.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s = "0123456789ABCDEF" ; 
+void Hex(long long n) {
+	if (n == 0) return;
+	Hex(n / 16);
+	int r = n % 16;
+	cout << r ; 
+}
+
+int main() {
+	long long n;
+	cin >> n;
+	if (n == 0)
+		cout << 0;
+	else
+		Hex(n);
+}
