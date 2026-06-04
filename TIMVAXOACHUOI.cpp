66563fa71e52c7df159714b34ac8c2baa154c774
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-06-04 20:32:52 +0700

    TIMVAXOACHUOI

--- TIMVAXOACHUOI.cpp
+++ TIMVAXOACHUOI.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
-using namespace std ; 
+using namespace std;
 int main(){
-	string s , t ; 
-	getline(cin , s ) ;
-	getline(cin , t ) ;
-	while(s.find(t) != string::npos){
-		s.erase(s.find(t) , s.find(t) + s.size() - 1 ) ; 
+	string s , t;
+	getline(cin,s);
+	getline(cin,t);
+	size_t pos;
+	while((pos = s.find(t)) != string::npos){
+		s.erase(pos,t.size());
+		size_t _pos ; 
+		while((_pos = s.find("  ")) != string::npos){
+			s.replace(_pos,2, " ");
+		}
 	}
-	cout << s ; 
+	cout << s;
+	return 0;
 }
