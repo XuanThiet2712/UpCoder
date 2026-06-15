71ad3f8273eaa9f144a53e7ef936c32775f741fd
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-06-15 23:57:04 +0700

    bai 5 on tap

---
+++ On_LTCB5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ; 
+int main(){
+	int n ; cin >> n ; 
+	int a[n] ; 
+	for(int &x : a)
+		cin >> x ; 
+	int cnt = 1 ; 
+	for(int i = 1 ; i < n ; i++){
+		if(a[i] > a[i-1]) cnt++;
+	}
+	cout << cnt ; 
+}
