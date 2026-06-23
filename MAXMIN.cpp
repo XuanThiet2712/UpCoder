c9ed5b01839cf42de13882c50b90c299fe38667b
XuanThiet2712 <sreaper2712thiet@gmail.com>
2026-06-23 23:41:23 +0700

    maxmin

---
+++ MAXMIN.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ; 
+int main(){
+	int n ; cin >> n ;
+	int max1 = 0, min1 = 9; 
+	while(n > 0){
+		int x = n % 10 ; 
+		if(x > max1 ) max1 = x ; 
+		if(x < min1 ) min1 = x ; 
+		n /= 10 ; 
+	}
+	cout << max1 + min1 ; 
+}
